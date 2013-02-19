20733b922e640018f638714e763f034e5f2e9967
valunt@gmail.com <>
2013-02-19 10:29:44 +0200

    qq

---
+++ src/javaapplication69/JavaApplication69.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication69;
+
+/**
+ *
+ * @author valtut
+ */
+public class JavaApplication69 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
